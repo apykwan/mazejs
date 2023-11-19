8ab5ae126bb4d52e30552952efcf9a6f844b5529
apykwan <apykwan@hotmail.com>
2023-11-18 22:42:29 -0800

    starting the project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { Engine, Render, Runner, World, Bodies } from 'matter-js'; 
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine,
+  options: {
+    width: 800,
+    height: 600
+  }
+});
+Render.run(render);
+Runner.run(Runner.create(),engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+  isStatic: true
+});
+
+World.add(world, shape)
