6faccc0ec388c2ae6ea3a42e3a33206fa5b90e03
apykwan <apykwan@hotmail.com>
2023-11-19 03:56:05 -0800

    testing matter-js

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { Engine, Render, Runner, World, Bodies } from 'matter-js'; 
+// https://brm.io/matter-js/docs/
+import { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } from 'matter-js'; 
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,15 +10,52 @@ const render = Render.create({
   element: document.body,
   engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes: false,
+    width,
+    height
   }
 });
 Render.run(render);
 Runner.run(Runner.create(),engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-  isStatic: true
-});
+// Allowing dragging the objects
+World.add(world, MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas)
+}));
 
-World.add(world, shape)
+// Walls
+// Bodieds.Rectangle(x, y, width, height, [options])
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+
+World.add(world, walls);
+
+const colorOfChoice = ['red', 'yellow', 'blue', 'green', 'purple', 'indigo', 'orange', 'brown', 'teal'];
+for (let i = 0; i < 50; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world, 
+      Bodies.rectangle(
+        Math.random() * width, 
+        Math.random() * height, 
+        50, 
+        50
+      )
+    );
+  } else {
+    World.add(
+      world, 
+      Bodies.circle(
+        Math.random() * width, 
+        Math.random() * height, 
+        35, 
+        { render: { fillStyle: colorOfChoice[Math.floor(Math.random() * colorOfChoice.length)] }
+      })
+    );
+  }
+  
+}
